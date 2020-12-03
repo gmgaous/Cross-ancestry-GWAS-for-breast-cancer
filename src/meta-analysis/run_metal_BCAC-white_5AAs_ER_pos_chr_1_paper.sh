17b2a9d9c91ae0f2abb6ac9b6c61f3f496d169bd
gmgaous <guiming@uchicago.edu>
2020-12-03 17:20:04 -0600

    Add files via upload

---
+++ src/meta-analysis/run_metal_BCAC-white_5AAs_ER_pos_chr_1_paper.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+module load gcc/6.2.0
+module load zlib/1.2.8
+module load metal/2011.03.25
+
+rm METAANALYSIS1.TBL
+rm METAANALYSIS1.TBL.info
+rm meta_analysis_BCAC-white_5AAs_ERpos_filtered_chr1.txt
+rm meta_analysis_BCAC-white_5AAs_ERpos_filtered_chr1_info.txt
+
+metal < metal_BCAC-white_5AAs_ER_pos_chr1_code_paper.txt  #metal_AABC_BCAC_ALL_filtered_chr1_code.txt #metal_AABC_BCAC_chr1.txt
+
+mv METAANALYSIS1.TBL meta_analysis_BCAC-white_5AAs_ERpos_filtered_chr1.txt
+mv METAANALYSIS1.TBL.info meta_analysis_BCAC-white_5AAs_ERpos_filtered_chr1_info.txt
+echo "finish chr1"
